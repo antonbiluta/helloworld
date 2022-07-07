9490acf0c899f48bcd343925164ac11944672cb7
antonbiluta <antonbiluta@gmail.com>
2022-07-07 22:53:44 +0300

    first commit

---
+++ Смешно.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("Смешно!\n");
+    return 0;
+}
---
+++ Смешно.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Смешно!";
+    return 0;
+}
